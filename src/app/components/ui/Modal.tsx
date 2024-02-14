5525f0974c7aa928c03f1fd9f18eb7c52a1ac18b
Sayed Mahmud <armanemon5801@gmail.com>
2024-02-15 02:54:09 +0600

    modal tag fixed

--- src/app/components/ui/Modal.tsx
+++ src/app/components/ui/Modal.tsx
@@ -2,11 +2,11 @@ import { ModalProps } from "../../../../types/global-interfaces";
 
 const Modal: React.FC<ModalProps> = ({ modalOpen, setModalOpen, children }) => {
   return (
-    <div className={`modal ${modalOpen ? "scale-100" : "scale-0"}`}>
-      <div className="modal-box relative bg-gray-300 p-1 rounded-lg">
+    <div className={`modal absolute ${modalOpen ? "scale-100" : "scale-0"}`}>
+      <div className="modal-box relative bg-gray-300 p-1 rounded-lg ">
         <label
           onClick={() => setModalOpen(false)}
-          className="bg-black text-slate-200 px-1.5 py-0.5 cursor-pointer rounded-full hover:bg-opacity-80 absolute right-2 ring-2"
+          className="bg-black text-slate-200 px-1.5 py-0.5 cursor-pointer rounded-full hover:bg-opacity-80 absolute right-2 ring-inherit"
         >
           ✕
         </label>
